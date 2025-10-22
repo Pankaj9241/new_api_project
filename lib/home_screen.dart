71e428d25f5b392fa3dbd1a7d9e81db0032e3a6e
Pankaj841219 <pankaj.edugaon@gmail.com>
2025-10-22 15:19:24 +0530

    i create screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(appBar: AppBar(title: Text("data")),
+      body: Column(),);
   }
 }
